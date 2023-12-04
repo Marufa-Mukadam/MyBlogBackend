b993735966306d3dbb561d7ea4932bc95258e176
Marufa-Mukadam <marufamukadam8661@gmail.com>
2023-12-04 18:28:47 +0530

    added

--- validation/PostValidation.js
+++ validation/PostValidation.js
@@ -3,10 +3,10 @@ import Joi from "joi"
 
 export const blogValidation = Joi.object({
     //removed this bcz it was giving problem while adding blog
-    // post_id: Joi.number().required(), 
+    post_id: Joi.number(), 
     title: Joi.string().required().min(2).max(256),
     description: Joi.string().min(8),
-    category:Joi.string().required().min(3),
+    category:Joi.string().min(3),
     img:Joi.string().allow('',null),
     date:Joi.string(),
     other_category:Joi.string().allow('',null)
